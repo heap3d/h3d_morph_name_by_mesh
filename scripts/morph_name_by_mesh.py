38231601db96eea48688ae9f75a1dc47171dba93
Dmytro Holub <heap3d@gmail.com>
2025-04-22 15:37:42 +0300

    initial commit

---
+++ scripts/morph_name_by_mesh.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# ================================
+# (C)2025 Dmytro Holub
+# heap3d@gmail.com
+# --------------------------------
+# modo python
+
+
+def main():
+    print('success')
+
+
+if __name__ == '__main__':
+    main()
